dfb09ce4d419d35d23172aef44ce0b8fd741d887
therealharpaljadeja <jadejaharpal14@gmail.com>
2022-05-30 17:21:19 +0530

    fix: suggested video thumbnail should redirect to video page

--- src/components/VideoDetails/SuggestedVideos.tsx
+++ src/components/VideoDetails/SuggestedVideos.tsx
@@ -13,90 +13,105 @@ import { LenstubePublication } from "src/types/local";
 dayjs.extend(relativeTime);
 
 const SuggestedVideos = () => {
-  const [videos, setVideos] = useState<LenstubePublication[]>([]);
-  const [pageInfo, setPageInfo] = useState<PaginatedResultInfo>();
-  const { loading, error, fetchMore } = useQuery(EXPLORE_QUERY, {
-    variables: {
-      request: {
-        sortCriteria: "TOP_COMMENTED",
-        limit: 10,
-        sources: [LENSTUBE_VIDEOS_APP_ID],
-      },
-    },
-    onCompleted(data) {
-      setPageInfo(data?.explorePublications?.pageInfo);
-      setVideos(data?.explorePublications?.items);
-    },
-  });
+	const [videos, setVideos] = useState<LenstubePublication[]>([]);
+	const [pageInfo, setPageInfo] = useState<PaginatedResultInfo>();
+	const { loading, error, fetchMore } = useQuery(EXPLORE_QUERY, {
+		variables: {
+			request: {
+				sortCriteria: "TOP_COMMENTED",
+				limit: 10,
+				sources: [LENSTUBE_VIDEOS_APP_ID],
+			},
+		},
+		onCompleted(data) {
+			setPageInfo(data?.explorePublications?.pageInfo);
+			setVideos(data?.explorePublications?.items);
+		},
+	});
 
-  const { observe } = useInView({
-    threshold: 1,
-    onEnter: () => {
-      fetchMore({
-        variables: {
-          request: {
-            cursor: pageInfo?.next,
-            sortCriteria: "TOP_COMMENTED",
-            limit: 10,
-            sources: [LENSTUBE_VIDEOS_APP_ID],
-          },
-        },
-      }).then(({ data }: any) => {
-        setPageInfo(data?.explorePublications?.pageInfo);
-        setVideos([...videos, ...data?.explorePublications?.items]);
-      });
-    },
-  });
-  if (loading) {
-    return <Loader />;
-  }
+	const { observe } = useInView({
+		threshold: 1,
+		onEnter: () => {
+			fetchMore({
+				variables: {
+					request: {
+						cursor: pageInfo?.next,
+						sortCriteria: "TOP_COMMENTED",
+						limit: 10,
+						sources: [LENSTUBE_VIDEOS_APP_ID],
+					},
+				},
+			}).then(({ data }: any) => {
+				setPageInfo(data?.explorePublications?.pageInfo);
+				setVideos([...videos, ...data?.explorePublications?.items]);
+			});
+		},
+	});
+	if (loading) {
+		return <Loader />;
+	}
 
-  return (
-    <div>
-      {!error && !loading && (
-        <>
-          <div className="space-y-3">
-            {videos?.map((video: LenstubePublication, index: number) => (
-              <div key={index} className="flex">
-                <div className="flex-none overflow-hidden rounded">
-                  <img
-                    src={getThumbnailUrl(video)}
-                    alt=""
-                    draggable={false}
-                    className="object-cover object-center h-24 w-44"
-                  />
-                </div>
-                <div className="flex items-start px-2.5">
-                  <div className="flex flex-col items-start flex-1 pb-1">
-                    <span className="flex w-full items-start justify-between space-x-1.5">
-                      <Link passHref href={`/videos/${video.id}`}>
-                        <a className="mb-1.5 text-sm font-medium line-clamp-2">
-                          {video.metadata?.name}
-                        </a>
-                      </Link>
-                    </span>
-                    <Link href={`/${video.profile?.handle}`}>
-                      <a className="text-xs hover:opacity-100 opacity-70">
-                        {video.profile?.handle}
-                      </a>
-                    </Link>
-                    <div className="flex items-center text-[11px] opacity-70 mt-0.5">
-                      <span>{dayjs(new Date(video.createdAt)).fromNow()}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          {pageInfo?.next && videos.length !== pageInfo?.totalCount && (
-            <span ref={observe} className="flex justify-center p-5">
-              <Loader />
-            </span>
-          )}
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{!error && !loading && (
+				<>
+					<div className='space-y-3'>
+						{videos?.map(
+							(video: LenstubePublication, index: number) => (
+								<div key={index} className='flex'>
+									<Link passHref href={`/videos/${video.id}`}>
+										<a>
+											<div className='flex-none overflow-hidden rounded'>
+												<img
+													src={getThumbnailUrl(video)}
+													alt=''
+													draggable={false}
+													className='object-cover object-center h-24 w-44'
+												/>
+											</div>
+										</a>
+									</Link>
+									<div className='flex items-start px-2.5'>
+										<div className='flex flex-col items-start flex-1 pb-1'>
+											<span className='flex w-full items-start justify-between space-x-1.5'>
+												<Link
+													passHref
+													href={`/videos/${video.id}`}>
+													<a className='mb-1.5 text-sm font-medium line-clamp-2'>
+														{video.metadata?.name}
+													</a>
+												</Link>
+											</span>
+											<Link
+												href={`/${video.profile?.handle}`}>
+												<a className='text-xs hover:opacity-100 opacity-70'>
+													{video.profile?.handle}
+												</a>
+											</Link>
+											<div className='flex items-center text-[11px] opacity-70 mt-0.5'>
+												<span>
+													{dayjs(
+														new Date(
+															video.createdAt
+														)
+													).fromNow()}
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							)
+						)}
+					</div>
+					{pageInfo?.next && videos.length !== pageInfo?.totalCount && (
+						<span ref={observe} className='flex justify-center p-5'>
+							<Loader />
+						</span>
+					)}
+				</>
+			)}
+		</div>
+	);
 };
 
 export default SuggestedVideos;
